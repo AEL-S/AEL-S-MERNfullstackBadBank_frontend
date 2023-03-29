d9ea9bb09492e5f31d9421faa0a831c27dcefb8e
Anna <105984477+AEL-S@users.noreply.github.com>
2023-03-29 10:54:41 -0500

    Update alldata.js

--- client/src/components/alldata.js
+++ client/src/components/alldata.js
@@ -4,26 +4,10 @@ import React from 'react';
 // import axios from 'axios';
 import UserContext from './context';
 import Card from 'react-bootstrap/Card';
-// import { useState } from "react";
-// import {useState, useEffect, useContext } from 'react'
-// import Button from 'react-bootstrap/Button';
-
 
     function AllData() {
       const ctx = React.useContext(UserContext);
-        // const [data, setData] = React.useState("");
-      
-        // React.useEffect(() => {
-        //   fetch("/account/all")
-        // // fetch("/alldata")
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //       setData(JSON.stringify(data));
-        //       console.log(data);
-        //     });
-        // }, []);
-      
-
+       
         return (
         // this is brining up errors in console but it is still working
         <div className="row row-cols-auto">
@@ -46,39 +30,13 @@ import Card from 'react-bootstrap/Card';
                               
                                   } 
                                   </Card.Body> 
-       </Card>
-          )
-                                }
-          )
-                              }
-               </div>
+                               </Card>
+                          )}
+                        )}
+                    </div>
                   )} 
           
 
                             
         export default AllData;
-// ---------------------------------------------------------------------
-        //  cant figure out why user and users keeps coming up undefined 
-
-
-// function AllData(){
-//     const [data, setData] = React.useState('');    
-    // const ctx = React.useContext(UserContext);
-    // const [user, setUser] = React.useState('');    
-    // const ctx = React.useContext(UserContext);
-
-
-    // below started throwing an error so I had to comment it out. look into it in the am
-    // React.useEffect(() => {
-        
-    //     // fetch all accounts from API
-    //     fetch('/account/all')
-    //     // fetch('/api/alldata')
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             console.log(data);
-    //             setData(JSON.stringify(data));                   
-    //         });
-
-    // }, []);
     
